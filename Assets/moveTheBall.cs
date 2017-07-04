9f6942005dc83fd4e35de679882f56b5f6ca93ad
myolisi <myolisi@ngubentombi.co.za>
2017-07-04 08:36:58 +0200

    Added the project to the repository.

---
+++ Assets/moveTheBall.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveTheBall : MonoBehaviour {
+
+    private Rigidbody b;
+    private Vector3 movement;
+    [HideInInspector] public float speed = 4.0f;
+	// Use this for initialization
+	void Start () {
+		
+        b = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+       
+
+        if (Input.GetKeyDown("space"))
+        {
+            Vector3 movement2 = new Vector3(horizontal, 120.0f, 0f); 
+            b.AddForce(movement2 * speed);
+        }
+        else
+        {
+            movement = new Vector3(horizontal, 0f, 0f);
+            b.AddForce(movement * speed);
+        }
+
+      
+	}
+}
