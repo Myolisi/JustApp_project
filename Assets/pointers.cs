f6805a08670af190e2da03e17574604d0ae2bd6f
myolisi <myolisi@ngubentombi.co.za>
2017-07-07 08:37:25 +0200

    no message

--- Assets/pointers.cs
+++ Assets/pointers.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class pointers : MonoBehaviour {
 
+    private Rigidbody rb;
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+    void OnTriggerEnter (Collider other) {
+        if (other.gameObject.CompareTag("trap"))
+        {
+            other.gameObject.SetActive(false);
+        }
 	}
 }
